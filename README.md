3f00264e251c4ad1474dde658914bae3aa34ec8e
System <none>
2017-09-26 11:41:20 +0000

    janets

--- README.md
+++ README.md
@@ -1,8 +1,56 @@
-# 1337 🤖
+# Janets
 
-A simple Discord bot that replies "damn it" whenever anyone says "1337"
+based off oliviacpu's Janets:
 
-1. Create an app here: [https://discordapp.com/developers/applications/me](https://discordapp.com/developers/applications/me)
-2. Click "Create a Bot User"
-3. Add the Bot User to your Discord server using this link: `https://discordapp.com/oauth2/authorize?&client_id=<CLIENT ID>&scope=bot&permissions=0` replacing `<CLIENT_ID>` with the Client ID found on the page of your app
-4. Set the `DISCORD_BOT_ID` value in `.env` using the Bot User token (Click to reveal in the app page)
+"I made a Discord bot but it's Janet.
+
+Maybe other Janets later."
+
+## Prerequisites
+
+You'll need:
+ - A [Discord account](https://discordapp.com/register)
+ - Permission to add accounts to a server, or your own server
+
+## Steps to set up:
+
+1. Create an App here: [https://discordapp.com/developers/applications/me](https://discordapp.com/developers/applications/me)
+2. Create an App Bot User for your App by clicking "Create a Bot User"
+3. Add the App Bot User to your Discord server using this link: `https://discordapp.com/oauth2/authorize?&client_id=<CLIENT ID>&scope=bot&permissions=0` replacing `<CLIENT_ID>` with the Client ID found on the page of your App
+4. Set the `DISCORD_BOT_ID` value in `.env` using the Bot User token ("click to reveal" in the App page under the bot username)
+
+## The code
+
+Check out `server.js` to see how it works ✨
+
+We're using the [Eris](https://npm.im/eris) library to interact with the Discord API.
+
+If it's working you should see "Bing!" in the logs!
+
+I also added an `express` server and a couple of `pug` templates to serve so that the circle would stop spinning.
+
+I'm still learning how bot actions and commands work.
+
+A pattern I'm thinking about to add commands and supporting listeners is to have a function that takes the `CommandClient` and then does whatever `registerCommand` or `on('event')` listening it needs for the behavior or action there.
+
+Kind of like how the bot actions in `oliviabot` work.
+
+## Contributing
+
+If you're in the Discord then ask me to invite you to edit this
+
+## Things to do
+
+These are some things I want to get Janet to do.
+
+- [x] Hang out in Janet's Void
+- [x] Prompt new members to introduce themselves
+- [x] Sometimes find a cactus I guess
+- [ ] Answer questions about how my Discord works
+- [ ] Enforce rules?
+- [ ] Coordinate things with friends?
+- [ ] Allow members to manage friend invites?
+
+Other things I want.
+
+- [ ] Private web config (purge rules per channel, enable/disable commands?)
--- server.js
+++ server.js
@@ -1,15 +1,154 @@
-const Eris = require('eris');
- 
-const bot = new Eris(process.env.DISCORD_BOT_TOKEN);   // Replace DISCORD_BOT_TOKEN in .env with your bot accounts token
- 
-bot.on('ready', () => {                                // When the bot is ready
-    console.log('Ready!');                             // Log "Ready!"
+const Eris = require("eris");
+
+const client = new Eris.CommandClient(
+  process.env.DISCORD_BOT_TOKEN,
+  {},
+  {
+    description: "PUWP Bot",
+    owner: "arael",
+    prefix: "."
+  }
+); // https://abal.moe/Eris/docs/CommandClient
+
+client.on("ready", () => {
+  // Janet exists
+  console.log("Bing!");
+  // Hang out in `channel` and turn off the mic indicator
+  // client.joinVoiceChannel('voicechannelid', {}).then(voice => voice.stopPlaying())
 });
- 
-bot.on('messageCreate', (msg) => {                     // When a message is created
-    if(msg.content === '1337') {                       // If the message content is "1337"
-        bot.createMessage(msg.channel.id, 'damn it');  // Send a message in the same channel with "damn it"
+
+// The one command we have that is reliable
+/*client.registerCommand("bot", (msg) => {
+  client.createMessage(msg.channel.id, 'Hi, I\'m Bot!')
+  msg.addReaction("✨")
+}, {
+   description: "Summon Bot",
+   fullDescription: "Hi, I'm Bot!"
+})*/
+
+client.registerCommand(
+  "commentator",
+  msg => {
+    msg.addReaction("✨");
+
+    console.log(msg.content);
+
+    var rolls = msg.member.roles;
+    console.log(rolls);
+
+    if (msg.member.roles.indexOf(process.env.RUNNER_ROLE_ID) > -1) {
+      msg.channel.guild.members.forEach(function(member) {
+        msg.mentions.forEach(function(element) {
+          if (member.user == element) {
+            msg.channel.guild.addMemberRole(
+              member.id,
+              process.env.COMMENTATOR_ROLE_ID
+            );
+            console.log("added commentator");
+            client.createMessage(
+              process.env.RUNNERS_CHANNEL_ID,
+              "Added commentator: " + element.username
+            );
+            return true;
+          }
+        });
+      });
     }
+  },
+  {
+    description: "Add commentator",
+    fullDescription:
+      "Add a commentator with this command by @-mentioning them in public (Runners Only)",
+    argsRequired: true,
+    deleteCommand: true
+  }
+);
+
+/*
+// Experimenting with other APIs
+const imgur = require('imgur')
+imgur.setClientId(process.env.IMGUR_CLIENT_ID)
+const { random } = require('lodash')
+client.registerCommand("cactus", (msg) => {
+  imgur.search("cactus")
+    .then((json) => {
+      const cacti = json.data
+      const cactus = cacti[random(0, cacti.length)]
+      client.createMessage(msg.channel.id, `Here you go! ${cactus.link}`)
+      msg.addReaction("🌵")
+    })
+    .catch((error) => {
+      console.error("Janet couldn't find a cactus?", error.message)
+    })
+
+}, {
+   description: "Summon a cactus",
+   fullDescription: "Here you go!"
+}) */
+
+const https = require("https");
+
+/*client.registerCommand('test', (msg) => {
+    
+  },
+  {
+    description: "Test",
+    fullDescription: "Test"
+  })
+*/
+
+// Need to figure out filter, permissions, etc. for purge
+/*client.registerCommand("purge", (request) => {
+  if (request.author.id === '153194092644270080') {
+    request.channel.createMessage(`I'm erasing all the messages.`)
+    request.channel.purge()
+  }
+}, {
+   description: "Purge all messages from the channel this is posted in (OLIVIA ONLY)",
+   fullDescription: "Purge all messages from the channel this is posted in (Only Olivia can use this right now)"
+})*/
+
+/*
+// Need to figure out filter, permissions, etc. for purge
+client.registerCommand("reboot", (request) => {
+  if (request.author.id === '153194092644270080') {
+    // Need to implement @emdantrim's idea for confirmation
+    // Post a message mentioning the user and asking to react with a specific emoji
+    // After seeing the emoji, delete the messages and post a confirmation emoji
+    // This way state (who requested, did they confirm, are we done) is pretty much all in Discord
+    
+    // My broke stuff
+    request.channel.createMessage(`I'm erasing <@${request.author.id}>'s messages from this channel, but other people will still remember.`)
+    // Limit doesn't work? Filter doesn't work? https://abal.moe/Eris/docs/TextChannel#function-purge
+    request.channel.purge(-1, (msg) => {
+      console.log(msg.author.id)
+      return msg.author.id === request.author.id
+    })
+  }
+}, {
+   description: "Purge all of your messages from the channel this is posted in (BROKEN)",
+   fullDescription: "Purge all of your messages from the channel this is posted in (BROKEN)"
+}) */
+
+client.connect();
+
+// Start an http server so the circle stops
+// Might add some web functionality like invite requests, or other stuff
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+app.set("view engine", "pug");
+
+app.get("/", function(request, response) {
+  response.render("index");
 });
- 
-bot.connect();                                         // Get the bot to connect to Discord
+
+app.use(function(request, response) {
+  response.status(404).render("404");
+});
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
